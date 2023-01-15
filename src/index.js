0b7dd6e40138d603adb111ad1a4e472eef6d432c
Tom Snelling <tomsnelling8@gmail.com>
2023-01-15 16:02:34 +0000

    adds time & weather display when not playing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,55 @@
 import PixelPusher from "node-pixel-pusher";
 import nodeCanvas, { loadImage } from "canvas";
 import fetch from "node-fetch";
+import moment from "moment";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const MAX_FPS = 30;
+
+const getWmoString = (code) => {
+  switch (code) {
+    case 0:
+    case 1:
+      return "Clear";
+    case 2:
+      return "Cloudy";
+    case 3:
+      return "Overcast";
+    case 45:
+    case 48:
+      return "Fog";
+    case 51:
+    case 53:
+    case 55:
+    case 56:
+    case 57:
+      return "Drizzle";
+    case 61:
+    case 63:
+    case 65:
+    case 66:
+    case 67:
+    case 80:
+    case 81:
+    case 82:
+      return "Rain";
+    case 71:
+    case 73:
+    case 75:
+    case 77:
+    case 85:
+    case 86:
+      return "Snow";
+    case 95:
+    case 96:
+    case 99:
+      return "Storms";
+    default:
+      return "";
+  }
+};
 
 function createRenderer(device) {
   const width = device.deviceData.pixelsPerStrip;
@@ -14,10 +58,11 @@ function createRenderer(device) {
   const ctx = canvas.getContext("2d");
 
   let image;
+  let weather;
 
   console.log(`Creating renderer ${width}x${height} ${MAX_FPS}fps`);
 
-  const getImage = async () => {
+  const getNowPlayingImage = async () => {
     const res = await fetch(
       `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${process.env.LASTFM_USER}&api_key=${process.env.LASTFM_KEY}&format=json&limit=1`
     );
@@ -33,8 +78,18 @@ function createRenderer(device) {
     }
   };
 
-  getImage();
-  setInterval(getImage, 15000);
+  const getWeather = async () => {
+    const res = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${process.env.WEATHER_LAT}&longitude=${process.env.WEATHER_LON}&current_weather=true`
+    );
+    const json = await res.json();
+    weather = json.current_weather;
+  };
+
+  getNowPlayingImage();
+  getWeather();
+  setInterval(getNowPlayingImage, 15000);
+  setInterval(getWeather, 5 * 60000);
 
   device.startRendering(() => {
     if (image) {
@@ -42,6 +97,16 @@ function createRenderer(device) {
     } else {
       ctx.fillStyle = "black";
       ctx.fillRect(0, 0, width, height);
+
+      ctx.fillStyle = "white";
+      ctx.font = "11px monospace";
+      ctx.fillText(moment().format("ddd Do"), 4, 11);
+      ctx.fillText(moment().format("HH:mm:ss"), 4, 22);
+
+      if (weather) {
+        ctx.fillText(`${weather.temperature}°C`, 4, 38);
+        ctx.fillText(getWmoString(weather.weathercode), 4, 49);
+      }
     }
     const ImageData = ctx.getImageData(0, 0, width, height);
     device.setRGBABuffer(ImageData.data);
